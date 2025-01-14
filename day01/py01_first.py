f77a09cce81d5d63ecccd0d0354c7bb8bee996d3
DongKuk-Kim <yum0153@gmail.com>
2025-01-14 17:11:52 +0900

    마무리정리

--- day01/py01_first.py
+++ day01/py01_first.py
@@ -1,4 +1,4 @@
-# Iot과정 첫번째 프로그램파일
+# Iot과정 첫번째 프로그램파일 - 주석 
 
-if __name__=='__main__':
-    print("hello, Python!")
+if __name__=='__main__': # 조건문, __name__과 '__main__'이 같다면.
+    print("hello, Python!") #hello, python() 함수로 실행하라
